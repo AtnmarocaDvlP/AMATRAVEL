3542cbc859533b9588d9bacc9a6a5ec17c45d8a8
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2026-04-25 14:04:22 -0300

    adding updated db

--- data/AMATRAVEL.sql
+++ data/AMATRAVEL.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `amatravel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `amatravel`;
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
 -- Host: localhost    Database: amatravel
@@ -23,13 +25,13 @@ DROP TABLE IF EXISTS `pessoa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pessoa` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(35) DEFAULT NULL,
-  `valor_ida` float(3,2) DEFAULT NULL,
-  `valor_volta` float(3,2) DEFAULT NULL,
+  `valor_ida` float(4,2) DEFAULT NULL,
+  `valor_volta` float(4,2) DEFAULT NULL,
   `ativo` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +40,38 @@ CREATE TABLE `pessoa` (
 
 LOCK TABLES `pessoa` WRITE;
 /*!40000 ALTER TABLE `pessoa` DISABLE KEYS */;
+INSERT INTO `pessoa` VALUES (2,'Julimar Melo',0.00,16.00,0),(3,'Izzabela',3.25,4.25,1),(4,'Lucas Barbosa',3.50,3.50,1),(5,'Matheus Zinato',3.25,3.25,1),(11,'Edson Ramos',3.25,0.00,1),(12,'Arthur',3.25,3.25,1),(14,'alexandre Maroca',3.50,4.50,0),(15,'Passageiro Teste',15.00,20.00,0),(16,'Antonio Maroca Amarante',2.50,2.50,0);
 /*!40000 ALTER TABLE `pessoa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `viagem_passageiro`
+--
+
+DROP TABLE IF EXISTS `viagem_passageiro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `viagem_passageiro` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `viagem_id` int DEFAULT NULL,
+  `pessoa_id` int DEFAULT NULL,
+  `pago` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `viagem_id` (`viagem_id`),
+  KEY `pessoa_id` (`pessoa_id`),
+  CONSTRAINT `viagem_passageiro_ibfk_1` FOREIGN KEY (`viagem_id`) REFERENCES `viagens` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `viagem_passageiro_ibfk_2` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `viagem_passageiro`
+--
+
+LOCK TABLES `viagem_passageiro` WRITE;
+/*!40000 ALTER TABLE `viagem_passageiro` DISABLE KEYS */;
+INSERT INTO `viagem_passageiro` VALUES (1,1,3,0),(2,2,5,0),(3,3,4,0),(4,4,11,0),(5,4,12,0);
+/*!40000 ALTER TABLE `viagem_passageiro` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -50,14 +83,12 @@ DROP TABLE IF EXISTS `viagens`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `viagens` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `pessoa_id` int DEFAULT NULL,
   `Ida` tinyint(1) DEFAULT NULL,
   `volta` tinyint(1) DEFAULT NULL,
-  `pago` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `pessoa_id` (`pessoa_id`),
-  CONSTRAINT `viagens_ibfk_1` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoa` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `data_viagem` date DEFAULT NULL,
+  `ativo` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,8 +97,13 @@ CREATE TABLE `viagens` (
 
 LOCK TABLES `viagens` WRITE;
 /*!40000 ALTER TABLE `viagens` DISABLE KEYS */;
+INSERT INTO `viagens` VALUES (1,1,0,'2026-04-22',1),(2,0,1,'2026-04-22',1),(3,1,0,'2026-04-22',0),(4,1,1,'2026-04-22',1);
 /*!40000 ALTER TABLE `viagens` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'amatravel'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -78,4 +114,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-27 13:12:24
+-- Dump completed on 2026-04-25 13:54:16
